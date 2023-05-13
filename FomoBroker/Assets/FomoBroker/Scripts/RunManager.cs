61264687bd8279488a1a9bb9370fa6f0e51332b6
Rikard <rikkypikki@hotmail.com>
2023-05-13 15:57:35 +0200

    Running visuals

---
+++ FomoBroker/Assets/FomoBroker/Scripts/RunManager.cs
@@ -0,0 +1,186 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Runner {
+    public Transform transform;
+    public int baseIndex;
+
+    public bool isRunning = false;
+    public Vector3 targetPosition;
+    public Animation anim;
+
+    public List<Vector3> waypoints = new List<Vector3>();
+    public int waypointIndex = 0;
+
+    public float runSpeed;
+}
+
+public class RunManager : MonoBehaviour
+{
+    public GameObject dudePrefab;
+
+    public Transform[] bases;
+    public Transform[] roads;
+    float baseRadius = 10.0f;
+
+    List<Runner> runners = new List<Runner>();
+    int[] runnerCountAtBase = new int[3];
+
+    bool runnersAreRunning = false;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        SpawnDudes(new int[]{ 30, 50, 10 });
+    }
+
+    void SpawnDudes(int[] numForBase) {
+        if(numForBase.Length != bases.Length) {
+            Debug.LogError("numForBase length must match bases length");
+            return;
+        }
+        for(int baseIndex = 0; baseIndex < numForBase.Length; baseIndex++) {
+            runnerCountAtBase[baseIndex] = numForBase[baseIndex];
+            Vector3 basePos = bases[baseIndex].position;
+            for(int ii = 0; ii < numForBase[baseIndex]; ++ii) {
+                GameObject go = GameObject.Instantiate(dudePrefab);
+                Vector2 randomOffset = Random.insideUnitCircle * baseRadius;
+                go.transform.position = basePos + new Vector3(randomOffset.x, 0, randomOffset.y); 
+
+                Runner runner = new Runner();
+                runner.transform = go.transform;
+                runner.baseIndex = baseIndex; 
+
+                runner.anim = runner.transform.GetComponent<Animation>();
+                runner.anim.Play("Armature|idle");
+
+                runner.runSpeed = Random.Range(25.0f, 35.0f);
+                runner.anim["Armature|run"].speed = 4.0f * runner.runSpeed / 30.0f;
+                runners.Add(runner);
+            }
+        }
+    }
+
+    const int BASE_COUNT = 3;
+
+    void Run(int[][] shiftCount) {
+        if(runnersAreRunning) return;
+
+        List<Runner>[] runnersAtBase = new List<Runner>[BASE_COUNT];
+        for(int ii = 0; ii < BASE_COUNT; ++ii) {
+            runnersAtBase[ii] = new List<Runner>();
+        }
+        for(int ri = 0; ri < runners.Count; ri++) {
+            Runner runner = runners[ri];
+            runnersAtBase[runner.baseIndex].Add(runner);
+        }
+
+        for(int bi = 0; bi < BASE_COUNT; ++bi) {
+            for(int si = 0; si < BASE_COUNT-1; ++si) {
+                int targetBaseIndex = (bi + si + 1) % BASE_COUNT; 
+                int roadIndex = ((bi + 1)%bases.Length == targetBaseIndex) ? bi : (bi+2)%bases.Length;
+                for(int ii = 0; ii < shiftCount[bi][si]; ++ii) {
+                    int runnerIndex = Random.Range(0, runnersAtBase[bi].Count);
+                    Runner runner = runnersAtBase[bi][runnerIndex];
+                    Debug.Assert(runner.baseIndex == bi);
+                    runnersAtBase[bi].RemoveAt(runnerIndex);
+                    
+                    runner.baseIndex = targetBaseIndex;
+
+                    runner.isRunning = true;
+                    float roadWidth = 10.0f;
+                    Vector3 p0 = roads[roadIndex].Find("p0").transform.position;
+                    Vector3 p1 = roads[roadIndex].Find("p1").transform.position;
+                    runner.waypoints.Clear();
+                    if(Vector3.Distance(p0, runner.transform.position) > Vector3.Distance(p1, runner.transform.position)) {
+                        Vector3 temp = p0;
+                        p0 = p1;
+                        p1 = temp;
+                        
+                    }
+
+                    float baseRadius = 10.0f;
+                    Vector3 roadDir = Vector3.Normalize((p1 - p0));
+                    Vector3 roadSide = Vector3.Cross(roadDir, Vector3.up);
+                    p0 += roadSide * Random.Range(-0.5f, 0.5f) * roadWidth;
+                    p1 += roadSide * Random.Range(-0.5f, 0.5f) * roadWidth;
+                    runner.waypoints.Add(p0);
+                    runner.waypoints.Add(p1);
+                    //float randomAngle = Random.Range()
+                    Vector2 randomOffset = Random.insideUnitCircle * baseRadius;
+                    runner.waypoints.Add(bases[targetBaseIndex].position + new Vector3(randomOffset.x, 0, randomOffset.y));
+
+                    runner.waypointIndex = 0;
+                    runner.targetPosition = runner.waypoints[runner.waypointIndex];
+
+                    runner.anim.CrossFade("Armature|run");
+                    runner.transform.Translate(new Vector3(0, 0, 30*Time.deltaTime));
+                }
+            }
+        }
+
+        runnersAreRunning = true;
+        /*for(int ri = 0; ri < runners.Count; ri++) {
+            Runner runner = runners[ri];
+            int targetBaseIndex = Random.Range(0, bases.Length);
+            if(targetBaseIndex == runner.baseIndex) continue;
+
+
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!runnersAreRunning && Input.GetKeyDown(KeyCode.W)) {
+            int[][] shiftCount = new int[3][];
+            for(int ii = 0; ii < 3; ++ii) {
+                shiftCount[ii] = new int[2];
+                for(int jj = 0; jj < 2; ++jj) {
+                    shiftCount[ii][jj] = Random.Range(0, runnerCountAtBase[ii]);
+                    //shiftCount[ii][jj] = runnerCountAtBase[ii];
+                    runnerCountAtBase[ii] -= shiftCount[ii][jj];                    
+                }
+            }
+            for(int ii = 0; ii < 3; ++ii) {
+                for(int jj = 0; jj < 2; ++jj) {
+                    runnerCountAtBase[(ii+jj+1)%BASE_COUNT] += shiftCount[ii][jj];
+                }
+            }
+            Run(shiftCount);
+        }
+
+        int remainingRunners = 0;
+        for(int ri = 0; ri < runners.Count; ++ri) {
+            Runner runner = runners[ri];
+            if(runner.isRunning) {
+                remainingRunners++;
+                Vector3 dp = runner.targetPosition - runner.transform.position;
+                float dist = dp.magnitude;
+                Vector3 dir = dp / dist;
+                float targetRotation = Mathf.Atan2(dir.x, dir.z);
+                float rotation = Mathf.MoveTowardsAngle(runner.transform.eulerAngles.y, targetRotation*Mathf.Rad2Deg, Time.deltaTime*360.0f*2.0f);
+                runner.transform.eulerAngles = new Vector3(0, rotation, 0);
+                float moveAmount = runner.runSpeed * Time.deltaTime;
+                if(dist <= moveAmount) {
+                    runner.waypointIndex++;
+                    if(runner.waypointIndex < runner.waypoints.Count) {
+                        runner.targetPosition = runner.waypoints[runner.waypointIndex];
+                    }
+                    else {
+                        runner.isRunning = false;
+                        runner.transform.position = runner.targetPosition;
+                        runner.anim.CrossFade("Armature|idle");
+
+                        Debug.Assert(Vector3.Distance(runner.transform.position, bases[runner.baseIndex].position) < 15.0f);
+                    }
+                }
+                else {
+                    runner.transform.position = runner.transform.position + dir * moveAmount;
+                }
+            }
+        }
+        if(remainingRunners == 0) runnersAreRunning = false;
+    }
+}
